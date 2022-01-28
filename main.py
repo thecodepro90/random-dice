7b2dba736d4d90abecee60d19fdfa7836b40b932
thecodepro90 <johnglopezzz@gmail.com>
2022-01-27 20:11:39 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+numofdice = int(input("Number of dice: "))
+dice = 0
+if numofdice > 10:
+    print("Too many dice!")
+else:
+    for dicenum in range(numofdice):
+        dicenum = random.randint(0,6)
+        dice+=1
+        print(f"Dice {dice}: {dicenum} ")
